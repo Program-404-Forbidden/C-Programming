58e0d28b18285531fa1b19d66ad0241a8e0a0e06
Program-404-Forbidden <smrakib200@gmail.com>
2023-03-07 00:23:22 +0600

    Added if else program

---
+++ if-else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d%d", &A, &B);
+    if (A > B)
+    {
+        printf("%d is Greater", A);
+    }
+    else
+    {
+        printf("%d is Greater", B);
+    }
+    return 0;
+}
